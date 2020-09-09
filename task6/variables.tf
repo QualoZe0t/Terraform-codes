f60bc53fc3908b5145d538c0a1b2f2a68da1ba65
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2020-09-09 16:39:25 +0530

    Update variables.tf

--- task6/variables.tf
+++ task6/variables.tf
@@ -1,1 +1,15 @@
-
+variable "rdspasswd" {
+  type = string
+  default = "podpasswd"
+  description = "Password for AWS-RDS MySQL Database"
+}
+variable "rdsusername" {
+  type = string
+  default = "admin"
+  description = "Username for database"
+}
+variable "port" {
+  type = string
+  default = "30029"
+  description = "Username for database"
+}
