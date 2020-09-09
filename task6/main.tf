dcd3dbeab46508751e97c1ce9043d73a77796156
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2020-09-09 16:39:47 +0530

    Update main.tf

--- task6/main.tf
+++ task6/main.tf
@@ -1,1 +1,24 @@
+module "awsrds" {
+     source = "./awsrds"
+     rdspasswd = var.rdspasswd
+     rdsusername = var.rdsusername
+}
 
+module "k8swp" {
+     source = "./k8swp"
+   
+     rdsendpoint = module.awsrds.rds_dbhost
+     rdsusername = var.rdsusername
+     rdsdbname = module.awsrds.rds_dbname
+     rdspasswd = var.rdspasswd
+     port = var.port
+}
+
+resource "null_resource" "wppod" {
+  depends_on = [
+    module.k8swp
+  ]
+  provisioner "local-exec" {
+    command = "firefox 192.168.99.100:${var.port}"
+  }
+}
