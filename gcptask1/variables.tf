02e905f7becd895c9ca5998b325a51081d4f6d92
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2020-09-18 19:00:09 +0530

    Create variables.tf

---
+++ gcptask1/variables.tf
@@ -0,0 +1,15 @@
+variable "devvpcid" {
+    default = "development-task-29"
+}
+variable "prodvpcid" {
+    default = "production-task-29"
+}
+variable "dbuser" {
+    default = "akhil"
+}
+variable "dbpass" {
+    default = "akhilpass"
+}
+variable "dbname" {
+    default = "mysqldb"
+}
