c05a7724bc1bc28137d24dcdbc7e7da4dda9b29e
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2020-09-21 21:26:55 +0530

    main.tf

--- gcptask1/main.tf
+++ gcptask1/main.tf
@@ -1,18 +1,31 @@
 module "gcpdev" {
      source = "./development"
      devvpcid = var.devvpcid
-    
-    // prodvpcid = module.gcpprod.vpcid
-    // dbip = module.gcpprod.dbip
-   //  dbuser = var.dbuser
-    // dbname = var.dbname
-   //  dbpass = var.dbpass
+     username = var.username
+     password = var.password
+     prodvpcid = module.gcpprod.vpcid
 }
-//module "gcpprod" {
-   //  source = "./production"
-  //   devvpcid = module.gcpdev.vpcid
-   //  prodvpcid = var.prodvpcid
-//     dbuser = var.dbuser
- //    dbname = var.dbname
-   //  dbpass = var.dbpass
-//}
+
+module "gcpprod" {
+     source = "./production"
+     devvpcid = module.gcpdev.vpcid
+     prodvpcid = var.prodvpcid
+     dbuser = var.dbuser
+     dbname = var.dbname
+     dbpass = var.dbpass
+}
+
+module "k8s" {
+   source = "./devk8s"
+   dbuser                 = var.dbuser    
+   dbname                 = var.dbname
+   dbpass                 = var.dbpass
+   username               = var.username
+   password               = var.password
+   dbip                   = module.gcpprod.dbip
+   nodepool               = module.gcpdev.nodepool
+   host                   = module.gcpdev.host
+   client_certificate     = module.gcpdev.client_certificate
+   client_key             = module.gcpdev.client_key
+   cluster_ca_certificate = module.gcpdev.cluster_ca_certificate
+}
